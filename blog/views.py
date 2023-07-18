48dc2d91735bff4f35625c85f8437fd5e6cb65e0
JM Bae <jungman82@gmail.com>
2023-07-18 12:55:12 +0900

    오타 수정

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from blog.models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts', posts})
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, id):
