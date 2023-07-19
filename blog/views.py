910b4748ddbf467fcf3e749af8f70c6f7633571a
JM Bae <jungman82@gmail.com>
2023-07-19 13:19:24 +0900

    [feat] 클래스 형 뷰 추가, 함수형 뷰(post_list) 경로 변경

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 # from django.http import Http404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 # Create your views here.
