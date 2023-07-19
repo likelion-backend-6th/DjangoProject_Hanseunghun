7b7b12d8a143af6b673e5465260941c56646950b
JM Bae <jungman82@gmail.com>
2023-07-19 11:35:32 +0900

    [feat] 페이지네이션 오류 처리, 마지막 페이지 orphans 옵션 추가

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 # from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
@@ -10,8 +10,13 @@ def post_list(request):
     per_page = request.GET.get('per_page', 3)
     page_number = request.GET.get('page', 1)
     post_list = Post.published.all()
-    paginator = Paginator(post_list, per_page)
-    posts = paginator.page(page_number)
+    paginator = Paginator(post_list, per_page, orphans=1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
